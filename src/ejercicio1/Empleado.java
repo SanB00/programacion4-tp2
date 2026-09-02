7707387691b58383469a69b1eea1207bb62f1a31
SanB00 <151892199+SanB00@users.noreply.github.com>
2026-09-02 10:05:04 -0300

    de Macarena Gottig - martes, 1 de septiembre de 2026, 12:25 Comencé creando el proyecto java, agregué el paquete ejercicio1 y copié, en él, las clases Persona y Empleado del TP N°1.
     TP2_GRUPO_16_v1.zip

---
+++ src/ejercicio1/Empleado.java
@@ -0,0 +1,55 @@
+package ejercicio1;
+
+import java.time.LocalDate;
+
+public class Empleado extends Persona {
+	
+	   // Atributos
+
+		private final int legajo;
+		private String puesto;
+		
+		private static int contLegajo = 999;
+		
+		// Constructores
+	
+		public Empleado() {
+			super();
+			legajo = 0;
+			puesto = " ";
+		}
+
+		public Empleado(String dni, String nombre, String apellido, LocalDate fechaNacimiento, String genero, String direccion, String telefono, String email, String puesto) 
+		{
+			super(dni, nombre, apellido, fechaNacimiento, genero, direccion, telefono, email);
+			this.puesto = puesto;
+			legajo = ++contLegajo;
+		}
+		
+		// Metodo
+
+		public static int devuelveProximoLegajo() {
+			return contLegajo + 1;
+		}
+
+		// Getters y Setters
+		public int getLegajo() {
+			return legajo;
+		}
+
+		public String getPuesto() {
+			return puesto;
+		}
+
+		public void setPuesto(String puesto) {
+			this.puesto = puesto;
+		}
+
+		// ToString
+
+		@Override
+		public String toString() {
+			return "EMPLEADO legajo: " + legajo + ", puesto: " + puesto + " | " + super.toString();
+		}
+
+}
