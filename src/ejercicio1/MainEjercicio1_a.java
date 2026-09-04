c6d48159d8f079a004992886d8e8f9cdd7eca268
SantiB <151892199+SanB00@users.noreply.github.com>
2026-09-04 12:47:14 -0300

    de Macarena Gottig - viernes, 4 de septiembre de 2026, 12:19 Buenos días! Adjunto el punto 4b. Para realizar ambas pruebas y evitar duplicar el código, extraje la lógica realizada por mi compañero al método "verificarPersona" en la misma clase main. De esta forma, solo tuve que llamarlo y enviarle, por parámetro, cada dni.
     TP2_GRUPO_16v6.zip

--- src/ejercicio1/MainEjercicio1_a.java
+++ src/ejercicio1/MainEjercicio1_a.java
@@ -3,16 +3,26 @@ package ejercicio1;
 public class MainEjercicio1_a {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		verificarPersona("AA202020");
+		verificarPersona("20202020");
+	
+	}
+	
+	public static void verificarPersona(String dni) 
+	{
 		try {
-			Persona p1= new Persona();
-			p1.setDni("AA202020");
-			Persona.exVerificarDNI(p1.getDni());
+			Persona p= new Persona();
+			p.setDni(dni);
+			
+			Persona.exVerificarDNI(p.getDni());
+			
 			System.out.println("Persona agregada correctamente");
 		}
 		catch(ExVerificarDni ex){
 			System.out.println("Persona no agregada por no verificar el DNI");
 		}
+		
 	}
-
+ 
 }
