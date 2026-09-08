2e465b1a4d281824f3c392525226c1f731b00727
SantiB <151892199+SanB00@users.noreply.github.com>
2026-09-08 17:59:14 -0300

    de Lautaro Valentin Bravo - martes, 8 de septiembre de 2026, 12:43 Buenas tardes!!
    Avance con el tp y cree la clase "Polideportivo" que implementa las interfaces de Edificio e IInstalacionDeportiva. Únicamente le agregue sus atributos.
    
    image.png
    
     TP2_GRUPO_16v13.zip

---
+++ src/ejercicio2/Polideportivo.java
@@ -0,0 +1,7 @@
+package ejercicio2;
+
+public class Polideportivo implements Edificio, IInstalacionDeportiva {
+    private String nombre;
+    private int tipoInstalacion;
+    private double superficie;
+}
