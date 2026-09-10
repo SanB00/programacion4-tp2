73cc1ec6ef2de91c3b7e0c5abb5a3d23ea6dcec8
SantiB <151892199+SanB00@users.noreply.github.com>
2026-09-09 22:26:12 -0300

    de Macarena Gottig - miércoles, 9 de septiembre de 2026, 14:23 Hola! Para continuar con la clase polideportivo, desarrollé sus constructores y los métodos de las interfaces que implementa. Declaré "tipoInstalacion" como un valor fijo final dentro de la clase, aunque la consigna no dice nada al respecto, asumo que todos sus objetos deberían tener el mismo. Además, desarrollé únicamente el método set de "superficie" ya que get realizaría exactamente lo mismo que el método "getSuperficieEdificio()".
     TP2_GRUPO_16v14.zip

--- src/ejercicio2/Polideportivo.java
+++ src/ejercicio2/Polideportivo.java
@@ -1,7 +1,31 @@
 package ejercicio2;
 
 public class Polideportivo implements Edificio, IInstalacionDeportiva {
-    private String nombre;
-    private int tipoInstalacion;
+
+    private static final int tipoInstalacion = 1;
     private double superficie;
+    private String nombre;
+
+    Polideportivo() {
+    }
+
+    Polideportivo(double superficie, String nombre) {
+        this.superficie = superficie;
+        this.nombre = nombre;
+    }
+
+    @Override
+    public int getTipoInstalación() {
+        return tipoInstalacion;
+    }
+
+    @Override
+    public double getSuperficieEdificio() {
+        return superficie;
+    }
+
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+
 }
